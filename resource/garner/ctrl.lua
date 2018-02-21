047f4d5e76f7f65bb657299ba853f4ea8e75b085
NMS3RR <andrey-gorshkov@yandex.ru>
2018-02-21 09:50:57 +0300

    logs

--- resource/garner/ctrl.lua
+++ resource/garner/ctrl.lua
@@ -3,18 +3,18 @@ function config(map)
 
 end
 
-function map_copy_run_darkblue()
+function map_copy_run_garner()
 
 end
 
-function map_run_darkblue(map)
+function map_run_garner(map)
 
 end
 
-function after_enter_darkblue( role , map_copy )
+function after_enter_garner( role , map_copy )
 
 end
 
-function before_leave_darkblue( role )
+function before_leave_garner( role )
 
 end
